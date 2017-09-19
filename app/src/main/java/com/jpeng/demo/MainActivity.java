a7154fc5371831011185936ca7e7062f6314ed11
peng8350 <peng8350@gmail.com>
2017-09-19 16:09:18 +0800

    Updated Demo:Add TitleBar to open left or right menu

--- app/src/main/java/com/jpeng/demo/MainActivity.java
+++ app/src/main/java/com/jpeng/demo/MainActivity.java
@@ -1,7 +1,8 @@
 package com.jpeng.demo;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ListView;
@@ -10,30 +11,59 @@ import com.facebook.rebound.SpringConfig;
 import com.jpeng.jpspringmenu.MenuListener;
 import com.jpeng.jpspringmenu.SpringMenu;
 
-public class MainActivity extends AppCompatActivity implements MenuListener {
+public class MainActivity extends Activity implements MenuListener {
     SpringMenu menu;
+
+    TitleBar mTitleBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mTitleBar = (TitleBar) findViewById(R.id.title_bar);
+        //init SpringMenu
         menu = new SpringMenu(this, R.layout.view_menu);
         menu.setMenuListener(this);
         menu.setFadeEnable(true);
         menu.setDirection(SpringMenu.DIRECTION_RIGHT);
         menu.setChildSpringConfig(SpringConfig.fromOrigamiTensionAndFriction(181,10));
+        // init titlebar
+        mTitleBar.setLeftText("Left");
+        mTitleBar.setTitle(R.string.app_name);
+        mTitleBar.setBackgroundColor(Color.parseColor("#64b4ff"));
+        mTitleBar.setDividerColor(Color.GRAY);
+        mTitleBar.setTitleColor(Color.WHITE);
+        mTitleBar.setLeftTextColor(Color.WHITE);
+        mTitleBar.setActionTextColor(Color.WHITE);
+        mTitleBar.setLeftClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu.setDirection(SpringMenu.DIRECTION_LEFT);
+            }
+        });
+        mTitleBar.addAction(new TitleBar.Action() {
+            @Override
+            public String getText() {
+                return "Right";
+            }
+
+            @Override
+            public int getDrawable() {
+                return 0;
+            }
+
+            @Override
+            public void performAction(View view) {
+                menu.setDirection(SpringMenu.DIRECTION_RIGHT);
+                menu.openMenu();
+            }
+        });
+
         ListBean[] listBeen = {new ListBean(R.mipmap.menu5,"菜单选项一"),new ListBean(R.mipmap.menu1,"菜单选项二"), new ListBean(R.mipmap.menu2,"菜单选项三"),new ListBean(R.mipmap.menu3,"菜单选项四"),new ListBean(R.mipmap.menu4,"菜单选项五")};
         MyAdapter adapter = new MyAdapter(this, listBeen);
         ListView listView = (ListView) menu.findViewById(R.id.test_listView);
         listView.setAdapter(adapter);
 
-        findViewById(R.id.btn_open).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                menu.openMenu();
-            }
-        });
     }
 
     @Override
